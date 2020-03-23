137906dd05431ec06a1a11148e11956488302623
Kevin <863129201@qq.com>
2020-03-23 09:53:36 +0800

    framework init

---
+++ app/helpers.php
@@ -0,0 +1,49 @@
+<?php
+
+//对返回给前端前对数据进行格式处理
+use Illuminate\Contracts\Pagination\Paginator;
+
+function responseData($data = [], $message = '', $code = 200, $statusCode = 200)
+{
+    $res = [
+        'code' => $code,
+        'msg' => $message,
+        'data' => $data
+    ];
+
+    //分页特殊处理
+    if ($data instanceof Paginator) {
+        $data = $data->toArray();
+        $page = [
+            'current_page' => $data['current_page'],
+            'last_page' => $data['last_page'],
+            'per_page' => $data['per_page'],
+            'total' => $data['total']
+        ];
+
+        $res['data'] = $data['data'];
+        $res['pages'] = $page;
+    }
+    return response()->json($res)->setStatusCode($statusCode);
+}
+
+function responseMessage($message= '操作成功',$code = 200,$statusCode = 200){
+    $res = [
+        'code' => $code,
+        'msg' => $message,
+        'data' => []
+    ];
+
+    return response()->json($res)->setStatusCode($statusCode);
+}
+
+//function responseError($errors,$code = 400, $statusCode= 422){
+//    $res = [
+////		'code' => $code,
+//        'message' => 'The given data was invalid.',
+//        'errors' => $errors,
+////		'data' => []
+//    ];
+//
+//    return response()->json($res)->setStatusCode($statusCode);
+//}
